ef6929d9b3e1f3950d680691aec3ba216f15fcc0
iBobbyTS <ibobbyts@gmail.com>
2024-04-08 16:16:53 -0600

    Move products to backend

--- Backend/app.py
+++ Backend/app.py
@@ -17,58 +17,76 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-
-products = [
+products = product = [
     {
-        "id": 1,
-        "name": "Product 1",
-        "description": "Description for Product 1", "price": 10.99,
-        "image": 'images/product1.png'
-    }, {
-        "id": 2,
-        "name": "Product 2",
-        "description": "Description for Product 2", "price": 20.99,
-        "image": 'images/product2.jpg'
-    }, {
-        "id": 3,
-        "name": "Product 3",
-        "description": "Description for Product 3", "price": 10.99,
-        "image": 'images/product3.jpg'
-    }, {
-        "id": 4,
-        "name": "Product 4",
-        "description": "Description for Product 4", "price": 10.99,
-        "image": 'images/product4.jpg'
-    }, {
-        "id": 5,
-        "name": "Product 5",
-        "description": "Description for Product 5", "price": 10.99,
-        "image": 'images/product5.jpg'
-    }, {
-        "id": 6,
-        "name": "Product 6",
-        "description": "Description for Product 6", "price": 10.99,
-        "image": 'images/product6.jpg'
-    }, {
-        "id": 7,
-        "name": "Product 7",
-        "description": "Description for Product 7", "price": 10.99,
-        "image": 'images/product7.jpg'
-    }, {
-        "id": 8,
-        "name": "Product 8",
-        "description": "Description for Product 8", "price": 10.99,
-        "image": 'images/product8.jpg'
-    }, {
-        "id": 9,
-        "name": "Product 9",
-        "description": "Description for Product 9", "price": 10.99,
-        "image": 'images/product9.jpg'
-    }, {
-        "id": 10,
-        "name": "Product 10",
-        "description": "Description for Product 10", "price": 10.99,
-        "image": 'images/product10.jpg'
+        'id': 1,
+        'name': "Product 1",
+        'description': "Description for Product 1",
+        'price': 10.99,
+        'image': 'images/product1.png'
+    },
+    {
+        'id': 2,
+        'name': "Product 2",
+        'description': "Description for Product 2",
+        'price': 20.99,
+        'image': 'images/product2.jpg'
+    },
+    {
+        'id': 3,
+        'name': "Product 3",
+        'description': "Description for Product 3",
+        'price': 10.99,
+        'image': 'images/product3.jpg'
+    },
+    {
+        'id': 4,
+        'name': "Product 4",
+        'description': "Description for Product 4",
+        'price': 10.99,
+        'image': 'images/product4.jpg'
+    },
+    {
+        'id': 5,
+        'name': "Product 5",
+        'description': "Description for Product 5",
+        'price': 10.99,
+        'image': 'images/product5.jpg'
+    },
+    {
+        'id': 6,
+        'name': "Product 6",
+        'description': "Description for Product 6",
+        'price': 10.99,
+        'image': 'images/product6.jpg'
+    },
+    {
+        'id': 7,
+        'name': "Product 7",
+        'description': "Description for Product 7",
+        'price': 10.99,
+        'image': 'images/product7.jpg'
+    },
+    {
+        'id': 8,
+        'name': "Product 8",
+        'description': "Description for Product 8",
+        'price': 10.99,
+        'image': 'images/product8.jpg'
+    },
+    {
+        'id': 9,
+        'name': "Product 9",
+        'description': "Description for Product 9",
+        'price': 10.99,
+        'image': 'images/product9.jpg'
+    },
+    {
+        'id': 10,
+        'name': "Product 10",
+        'description': "Description for Product 10",
+        'price': 10.99,
+        'image': 'images/product10.jpg'
     }
 ]
 if os.path.exists('users.json'):
@@ -114,9 +132,9 @@ def login_api():
         return jsonify({'error': 0})  # Login success
 
 
-# @app.route('/register_api', methods=['GET'])
-# def register_api():
-#     return jsonify(products)
+@app.route('/products_api', methods=['GET'])
+def products_api():
+    return jsonify({'products': products})
 
 
 if __name__ == '__main__':
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,8 +1,21 @@
-// ProductList.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Product from './Product';
 
-const ProductList = ({ products, onAddToCart }) => {
+const ProductList = ({ onAddToCart }) => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function getProducts() {
+      try {
+        const response = await fetch('http://127.0.0.1:5000/products_api');
+        const data = await response.json();
+        setProducts(data.products);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    getProducts();
+  }, []);
   return (
     <div className="product-list">
       {products.map(product => (
